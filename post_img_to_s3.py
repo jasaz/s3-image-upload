055033cfa243a4eb2c52dc234d34031c0789e802
jasaz <sandip.pradhan@gmail.com>
2019-03-31 21:22:49 +0530

    Added comments

--- post_img_to_s3.py
+++ post_img_to_s3.py
@@ -1,3 +1,6 @@
+# Invokes API securely by using AWS Signature version 4 to hash the secret_key and access_key
+# The steps for signing AWS request can be found here: https://docs.aws.amazon.com/general/latest/gr/signing_aws_api_requests.html 
+
 import requests
 import base64
 import json
@@ -37,6 +40,7 @@ def getSignatureKey(key, dateStamp, regionName, serviceName):
     kSigning = sign(kService, 'aws4_request')
     return kSigning
 
+# Please add the access_key and secret_key from the credentials file, DO NOT HARD CODE.
 access_key = 'your_access_key'
 secret_key = 'your_secret_key'
 
@@ -44,26 +48,33 @@ if access_key is None or secret_key is None:
     print('No access key is available.')
     sys.exit()
 
+# Create a date for headers and the credential string
 t = datetime.datetime.utcnow()
 amz_date = t.strftime('%Y%m%dT%H%M%SZ')
 date_stamp = t.strftime('%Y%m%d')
 
+#Create a canonical request
 canonical_uri = '/prod/upload-image-to-s3'
 canonical_querystring = ''
 canonical_headers = 'content-type:' + content_type + '\n' + 'host:' + host + '\n' + 'x-amz-date:' + amz_date + '\n' + 'x-amz-target:' + amz_target + '\n'
 
+#Create the list of signed headers
 signed_headers = 'content-type;host;x-amz-date;x-amz-target'
 payload_hash = hashlib.sha256(request_parameters.encode('utf-8')).hexdigest()
 canonical_request = method + '\n' + canonical_uri + '\n' + canonical_querystring + '\n' + canonical_headers + '\n' + signed_headers + '\n' + payload_hash
 
+#Match the algorithm to the hashing algorithm you use SHA-256
 algorithm = 'AWS4-HMAC-SHA256'
 credential_scope = date_stamp + '/' + region + '/' + service + '/' + 'aws4_request'
 string_to_sign = algorithm + '\n' +  amz_date + '\n' +  credential_scope + '\n' +  hashlib.sha256(canonical_request.encode('utf-8')).hexdigest()
 
+# Create the signing key using the function defined above
+# Add signing information to the request
 signing_key = getSignatureKey(secret_key, date_stamp, region, service)
 signature = hmac.new(signing_key, (string_to_sign).encode('utf-8'), hashlib.sha256).hexdigest()
 authorization_header = algorithm + ' ' + 'Credential=' + access_key + '/' + credential_scope + ', ' +  'SignedHeaders=' + signed_headers + ', ' + 'Signature=' + signature
 
+#Form the Headers
 headers = {'Content-Type':content_type,
            'X-Amz-Date':amz_date,
            'X-Amz-Target':amz_target,
